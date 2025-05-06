39d9031e6079e2393bc6e595181a5b646b94b442
prteodoro <97711803+prteodoro@users.noreply.github.com>
2025-05-05 23:53:50 -0300

    Start new conversation

--- djangobnb_backend/chat/api.py
+++ djangobnb_backend/chat/api.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from .models import Conversation, ConversationMessage
 from .serializers import ConversationListSerializer, ConversationDetailSerializer, ConversationMessageSerializer
 
+from useraccount.models import User
 
 @api_view(['GET'])
 def conversations_list(request):
@@ -24,3 +25,19 @@ def conversations_detail(request, pk):
         'conversation': conversation_serializer.data,
         'messages': messages_serializer.data
     }, safe=False)
+
+@api_view(['GET'])
+def conversations_start(request, user_id):
+    conversations = Conversation.objects.filter(users__in=[user_id]).filter(users__in=[request.user.id])
+
+    if conversations.count() > 0:
+        conversation = conversations.first()
+
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
+    else:
+        user = User.objects.get(pk=user_id)
+        conversation = Conversation.objects.create()
+        conversation.users.add(request.user)
+        conversation.users.add(user)
+
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
